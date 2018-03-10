32510686fc15fda6848a4d2762bc2406dd626db4
perfyXu <xpf00800@163.com>
2018-03-11 01:35:36 +0800

    完成搜索页面的语音输入功能。

--- app/src/main/java/com/project/perfy/mobilemediaplayer/application/MyApplication.java
+++ app/src/main/java/com/project/perfy/mobilemediaplayer/application/MyApplication.java
@@ -2,6 +2,9 @@ package com.project.perfy.mobilemediaplayer.application;
 
 import android.app.Application;
 
+import com.iflytek.cloud.SpeechConstant;
+import com.iflytek.cloud.SpeechUtility;
+
 import org.xutils.BuildConfig;
 import org.xutils.x;
 
@@ -15,5 +18,10 @@ public class MyApplication extends Application {
         super.onCreate();
         x.Ext.init(this);
         x.Ext.setDebug(BuildConfig.DEBUG); // 是否输出debug日志，开启debug会影响性能。
+
+        //初始化科大讯飞
+        // 将“12345678”替换成您申请的 APPID，申请地址： http://www.xfyun.cn
+        // 请勿在“ =” 与 appid 之间添加任务空字符或者转义符
+        SpeechUtility.createUtility(this, SpeechConstant.APPID + "=5795c210");
     }
 }
